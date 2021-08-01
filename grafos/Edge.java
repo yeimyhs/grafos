78d870d54851d8a1c80257b1ac5f7863bb709906
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-01 16:03:18 -0500

    se hizo la clase arista

---
+++ grafos/Edge.java
@@ -0,0 +1,28 @@
+package grafos;
+
+public class Edge<E> {//arista
+	protected Vertex<E> refDest; //vertice del grafo
+	protected int weight;//el peso en caso de ser grafo ponderado
+
+	public Edge(Vertex<E> refDest) {//contructor
+		this(refDest, -1);//-1 en caso de no ser ponderado
+	}
+
+	public Edge (Vertex<E> refDest, int weight) {// constructor con peso de la arista
+		this.refDest = refDest;//destino
+		this.weight = weight;//peso
+	}
+
+	public boolean equals(Object o) {//sobreescribiendo etodo equals
+		if(o instanceof Edge<?>) {
+			Edge<E> e =(Edge<E>)o;//casteando el objeto
+			return this.refDest.equals(e.refDest);//devuelve si esigual
+		}
+		return false;//devuele false si no es igual
+	}
+
+	public String toString(){//sobre escribiendo metodo toString
+		if(this.weight > -1) return refDest.data +" ["+ this.weight +"], ";
+		else return refDest.data+ ", ";
+	}
+}
