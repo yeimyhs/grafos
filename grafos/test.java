61b2c4cc413a479bef9a5c2bb6a49be0a2445463
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-01 17:06:42 -0500

    testeando la funcionalidad de la clase grafo , es correcta

--- grafos/test.java
+++ grafos/test.java
@@ -2,16 +2,21 @@ package grafos;
 
 public class test {
 	public static void main(String[] args) {
-		ListaAdyacencia<String> la = new ListaAdyacencia();
-		la.insert("1");
-		la.insert("2");
-		la.insert("3");
-		la.insert("4");
-		la.insert("5");
-		la.insert("6");
-		System.out.println(la.counter);
-		System.out.println(la.search("5"));
-		System.out.println(la.toString());
+		GraphLink<String> la = new GraphLink<>();
+		la.insertVertex("lima");
+		la.insertVertex("aqp");
+		la.insertVertex("cusco");
+		la.insertVertex("piura");
+		la.insertVertex("tarapoto");
+		
+		la.insertEdge("lima", "aqp");
+		la.insertEdge("cusco", "aqp");
+		la.insertEdge("piura", "lima");
+		la.insertEdge("cusco", "lima");
+		la.insertEdge("piura", "tarapoto");
+		//la.insertEdge("lima", "aqp");
+		
+		System.out.println("Grafo ciudades :\n"+ la);
 		
 		
 	}
