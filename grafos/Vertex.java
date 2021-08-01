54d955d3b542d6ee59ee294477470297a5b8b402
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-01 16:13:35 -0500

    case vertice con constructores y metodos sobreescritos

---
+++ grafos/Vertex.java
@@ -0,0 +1,27 @@
+package grafos;
+
+public class Vertex<E> {//vertice
+	protected E data; //data generica
+	protected ListaAdyacencia<Edge<E>> listAdj;//creacion lista de aristas
+	
+	public Vertex(E data) {//constructor del vertice 
+		this.data = data;
+		listAdj = new ListaAdyacencia<Edge<E>>();//inicializando su lista de aristas
+	}
+	
+	public boolean equals (Object o) {//sobreescribiendo el metodo equals
+		if(o instanceof Vertex<?>) {
+			Vertex<E> v = (Vertex<E>)o;
+			return this.data.equals(v.data);
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {//sobreescribiendo el metodo toString
+		return this.data+ " ---> " + this.listAdj.toString()+"\n";
+	}
+	
+	
+
+}
