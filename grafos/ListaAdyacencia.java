9cf4a55f0f12ca76c572ca001655ad7aafb27ff1
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-08-01 16:02:07 -0500

    se creo la lista de adyacencia

---
+++ grafos/ListaAdyacencia.java
@@ -0,0 +1,50 @@
+package grafos;
+
+public class ListaAdyacencia<T> {
+
+	protected Node<T> head;
+	protected Node<T> last;
+	protected int counter;
+	
+	public ListaAdyacencia() {}
+	public ListaAdyacencia(Node<T> head) {
+		this.head = head;
+	}
+
+	public void insert(T data) {
+		Node<T> aux = new Node<T> (data); 
+		if (counter == 0) {
+			this.head = aux;
+			this.last = head;
+			counter++;
+		}else {
+			last.next = aux;
+			this.last = last.next;
+			counter ++;
+		}
+	}
+
+	public T search(T data) {
+		Node<T> aux = this.head;
+		System.out.println(aux.toString());
+		while(aux != null && !aux.data.equals(data)) {
+			aux = aux.next;
+		}
+		if(aux != null) {
+			return aux.data;
+		} else {
+			return null;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String str = "";
+		Node <T> aux = this.head;
+		while(aux != null) {
+			str += aux.toString()+ " "; 
+			aux = aux.next;
+		}
+		return str;
+	}
+}
